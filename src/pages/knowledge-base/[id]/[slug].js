27c67bad2e66e186f8912d8d5773233cb7c9604c
thomasnkurniawan <thomas.kurniawan@jublia.com>
2025-08-11 13:40:24 +0700

    feat: fix style button

--- src/pages/knowledge-base/[id]/[slug].js
+++ src/pages/knowledge-base/[id]/[slug].js
@@ -245,23 +245,24 @@ const AccordionContent = ({ item, index, title }) => {
                 }`}
                 aria-labelledby={content.title.replace(/\s+/g, "")}
               >
-                <div
-                  className="accordion-body"
-                  dangerouslySetInnerHTML={{ __html: content.content }}
-                ></div>
-                {content?.downloadButtons &&
-                  content?.downloadButtons?.map((value) => {
-                    return (
-                      <a
-                        href={value?.link}
-                        className="btn btn-success rounded-5 my-3 btn-download d-flex justify-content-center align-items-center w-50"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        {value?.text}
-                      </a>
-                    );
-                  })}
+                <div className="accordion-body">
+                  <div
+                    dangerouslySetInnerHTML={{ __html: content.content }}
+                  ></div>
+                  {content?.downloadButtons &&
+                    content?.downloadButtons?.map((value) => {
+                      return (
+                        <a
+                          href={value?.link}
+                          className="btn btn-success rounded-5 my-3 btn-download d-flex justify-content-center align-items-center w-50"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          {value?.text}
+                        </a>
+                      );
+                    })}
+                </div>
               </div>
             </div>
           </div>
